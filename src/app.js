0f98b712b6dd98703b7fb5f7aa08ed510b9e7ec9
MincheolCha <mc.cha@greenlabs.co.kr>
2022-12-12 19:59:05 +0900

    user MVC

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const routes = require("./routes");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to API Server!!");
+});
+app.use("/", routes);
+
+app.use((req, res, _next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(500).json({ error: err.message });
+});
 
 module.exports = app;
--- src/models/user.js
+++ src/models/user.js
@@ -39,7 +39,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: "user",
-      undunderscored: true,
+      underscored: true,
     }
   );
   return User;

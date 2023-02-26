bc3d20cdb517cf431e2c5ba34c4348b64c9b0f07
MincheolCha <mc.cha@greenlabs.co.kr>
2023-02-26 23:46:49 +0900

    temp

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,10 @@
+const { getLinkPreview } = require("link-preview-js");
+
+module.exports = {
+  getUrl: async (req, res) => {
+    console.log(req.query.url);
+    const response = await getLinkPreview(req.query.url);
+    res.send(response);
+  },
+};
 module.exports.userController = require("./user.controller");
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userRoutes = require("./user.route");
+const { getUrl } = require("../controllers");
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
+router.get("/url", getUrl);
 
 module.exports = router;
